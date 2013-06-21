b8da3ab27d815611e83bd8b48a8f7a56790a8a73
Dan S Turner <turner@nowwithmoredan.com>
2013-06-20 20:41:51 -0700

    pre-refactor

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,5 +1,13 @@
 class FizzBuzz
 	def fizzbuzz(number)
-		number
+		if number % 3 == 0 && number % 5 == 0
+			'fizzbuzz'
+		elsif number % 3 == 0
+			'fizz'
+		elsif number % 5 == 0
+			'buzz'
+	  else
+		  number
+	  end
 	end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -9,4 +9,16 @@ describe FizzBuzz do
   it 'should return number if not divisible by 3 or 5' do
     @fb.fizzbuzz(7).should eq 7
   end
+
+  it 'should return fizz if number is divisble by 3' do
+    @fb.fizzbuzz(9).should eq 'fizz'
+  end
+
+  it 'should return buzz if number is divisble by 5' do
+    @fb.fizzbuzz(10).should eq 'buzz'
+  end
+
+  it 'should return fizzbuzz if number is divisble by 3 and 5' do
+    @fb.fizzbuzz(15).should eq 'fizzbuzz'
+  end
 end
