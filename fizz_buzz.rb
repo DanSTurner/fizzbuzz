c53ed605dc68ebecdcaa2fd51c25944d7e860843
Dan S Turner <turner@nowwithmoredan.com>
2013-06-20 21:18:40 -0700

    refactor - I see why case is preferred

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,13 +1,10 @@
 class FizzBuzz
 	def fizzbuzz(number)
-		if number % 3 == 0 && number % 5 == 0
-			'fizzbuzz'
-		elsif number % 3 == 0
-			'fizz'
-		elsif number % 5 == 0
-			'buzz'
-	  else
-		  number
+		case
+		  when number % 15 == 0 then 'fizzbuzz'
+			when number % 3 == 0 then 'fizz'
+			when number % 5 == 0 then'buzz'
+		  else number
 	  end
 	end
 end
